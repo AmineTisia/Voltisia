b1a6751927f4a2002f8b8b83a0fb38b189fa824b
aminetisia <aminebenrkia@gmail.com>
2025-04-21 22:27:09 +0200

    Initial commit for Voltisia

---
+++ voltisia/src/pages/Chantiers.jsx
@@ -0,0 +1,5 @@
+function Chantiers() {
+  return <div className='p-4 text-white'>Page Chantiers</div>
+}
+
+export default Chantiers
---
+++ voltisia/src/pages/Clients.jsx
@@ -0,0 +1,5 @@
+function Clients() {
+  return <div className='p-4 text-white'>Page Clients</div>
+}
+
+export default Clients
